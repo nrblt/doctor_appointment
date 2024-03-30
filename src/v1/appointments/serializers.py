352a571e49c4430417bb40abf21f7f321a315b14
Nurbolat <nurboturbo.nt@gmail.com>
2024-03-30 17:58:26 +0600

    fix bug with doctor

--- src/v1/appointments/serializers.py
+++ src/v1/appointments/serializers.py
@@ -2,6 +2,7 @@ from rest_framework import serializers
 from .models import Appointment
 from drf_spectacular.utils import extend_schema_field
 
+
 class AppointmentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Appointment
@@ -20,13 +21,16 @@ class AppointmentListSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Appointment
-        fields = ['id', 'user', 'doctor', 'time', 'date', 'appointment_option', 'is_taken']
-    
+        fields = ['id', 'user', 'doctor', 'time',
+                  'date', 'appointment_option', 'is_taken']
+
     @extend_schema_field(bool)
     def get_is_taken(self, obj):
         """
         Check if the time slot is taken for the given date.
         """
-        taken_time_slots = Appointment.objects.filter(date=obj.date).values_list('time', flat=True)
-        available_time_slots = [choice[0] for choice in self.TIME_CHOICES if choice[0] not in taken_time_slots]
+        taken_time_slots = Appointment.objects.filter(
+            date=obj.date, doctor=obj.doctor).values_list('time', flat=True)
+        available_time_slots = [
+            choice[0] for choice in self.TIME_CHOICES if choice[0] not in taken_time_slots]
         return obj.time in available_time_slots
